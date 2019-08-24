589d718fae1fe0995dc716536744a1d64b66ae89
maozhihao <maozhihao@ddicar.com>
2019-08-24 17:22:57 +0800

    init

---
+++ src/main/java/com/shoufeng/ShoufengRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.shoufeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoufengRestfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoufengRestfulApiApplication.class, args);
+    }
+
+}
